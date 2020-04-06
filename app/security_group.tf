479c96dd5578f0d8582fe04b23da28200bc1a8a4
Marcos Rosse <marcosrossem@gmail.com>
2020-04-06 15:04:09 -0300

    added security_group.tf

---
+++ app/security_group.tf
@@ -0,0 +1,65 @@
+########
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Habilita ssh para todas as redes"
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0 
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+########
+
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+  description = "Habilita tls"
+  ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0 
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+########
+
+resource "aws_security_group" "allow_http" {
+  name = "allow_http"
+  description = "Habilita tls"
+  ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = 0 
+      to_port = 0
+      protocol = -1
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_http"
+  }
+}
